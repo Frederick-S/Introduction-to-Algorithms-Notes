29a7fea0d90cb3a1d404a6654c8d5ce62b9dcc87
Frederick-S <mao_xiaodan@hotmail.com>
2014-04-14 22:22:51 +0800

    Update code

--- Data-Structures-and-Algorithm-Analysis-in-C/Code/Maximum-Subsequence-Sum-Problem/js/maximum-subsequence-sum-problem.js
+++ Data-Structures-and-Algorithm-Analysis-in-C/Code/Maximum-Subsequence-Sum-Problem/js/maximum-subsequence-sum-problem.js
@@ -16,3 +16,20 @@ function solution1(numbers) {
 
     return max;
 }
+
+// 2. solution 2
+function solution2(numbers) {
+    var length = numbers.length, max = 0, sum = 0;
+    for (var i = 0; i < length; i++) {
+        sum = 0;
+        for (var j = i; j < length; j++) {
+            sum += numbers[j];
+
+            if (sum > max) {
+                max = sum;
+            }
+        }
+    }
+
+    return max;
+}
--- Data-Structures-and-Algorithm-Analysis-in-C/Code/Maximum-Subsequence-Sum-Problem/js/tests.js
+++ Data-Structures-and-Algorithm-Analysis-in-C/Code/Maximum-Subsequence-Sum-Problem/js/tests.js
@@ -1,7 +1,13 @@
-// 1. Solution1
+// 1. Solution 1
 test('Solution 1', function () {
     ok(solution1([4, -3, 5, -2, -1, 2, 6, -2]) == 11, 'The maximum subsequence sum of array [4, -3, 5, -2, -1, 2, 6, -2] is 11.');
     ok(solution1([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6, 'The maximum subsequence sum of array [−2, 1, −3, 4, −1, 2, 1, −5, 4] is 6.');
     ok(solution1([-2, -3, 4, -1, -2, 1, 5, -3]) == 7, 'The maximum subsequence sum of array [-2, -3, 4, -1, -2, 1, 5, -3] is 7.');
+});
 
+// 2. Solution 2
+test('Solution 2', function () {
+    ok(solution2([4, -3, 5, -2, -1, 2, 6, -2]) == 11, 'The maximum subsequence sum of array [4, -3, 5, -2, -1, 2, 6, -2] is 11.');
+    ok(solution2([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6, 'The maximum subsequence sum of array [−2, 1, −3, 4, −1, 2, 1, −5, 4] is 6.');
+    ok(solution2([-2, -3, 4, -1, -2, 1, 5, -3]) == 7, 'The maximum subsequence sum of array [-2, -3, 4, -1, -2, 1, 5, -3] is 7.');
 });
