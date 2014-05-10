5ceaf89978414b00dc90607e485bd6ecd5b861d0
Frederick-S <mao_xiaodan@hotmail.com>
2014-05-10 10:13:56 +0800

    Add solution 3

--- Data-Structures-and-Algorithm-Analysis-in-C/Code/Maximum-Subsequence-Sum-Problem/js/maximum-subsequence-sum-problem.js
+++ Data-Structures-and-Algorithm-Analysis-in-C/Code/Maximum-Subsequence-Sum-Problem/js/maximum-subsequence-sum-problem.js
@@ -33,3 +33,35 @@ function solution2(numbers) {
 
     return max;
 }
+
+// 3. Solution 3
+function solution3(numbers, left, right) {
+    var maxLeftSum = 0, maxRightSum = 0;
+    var maxLeftBorderSum = 0, maxRightBorderSum = 0;
+    var leftBorderSum = 0, rightBorderSum = 0;
+    var center = 0, i = 0, j = 0;
+
+    if (left === right) {
+        return numbers[left] > 0 ? numbers[left] : 0;
+    }
+
+    center = parseInt((left + right) / 2);
+    maxLeftSum = solution3(numbers, left, center);
+    maxRightSum = solution3(numbers, center + 1, right);
+
+    for (i = center; i >= left; i--) {
+        leftBorderSum += numbers[i];
+        if (leftBorderSum > maxLeftBorderSum) {
+            maxLeftBorderSum = leftBorderSum;
+        }
+    }
+
+    for (j = center + 1; j <= right; j++) {
+        rightBorderSum += numbers[j];
+        if (rightBorderSum > maxRightBorderSum) {
+            maxRightBorderSum = rightBorderSum;
+        }
+    }
+
+    return Math.max(maxLeftSum, maxRightSum, maxLeftBorderSum + maxRightBorderSum);
+}
--- Data-Structures-and-Algorithm-Analysis-in-C/Code/Maximum-Subsequence-Sum-Problem/js/tests.js
+++ Data-Structures-and-Algorithm-Analysis-in-C/Code/Maximum-Subsequence-Sum-Problem/js/tests.js
@@ -11,3 +11,10 @@ test('Solution 2', function () {
     ok(solution2([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6, 'The maximum subsequence sum of array [−2, 1, −3, 4, −1, 2, 1, −5, 4] is 6.');
     ok(solution2([-2, -3, 4, -1, -2, 1, 5, -3]) == 7, 'The maximum subsequence sum of array [-2, -3, 4, -1, -2, 1, 5, -3] is 7.');
 });
+
+// 3. Solution 3
+test('Solution 3', function () {
+    ok(solution3([4, -3, 5, -2, -1, 2, 6, -2], 0, 7) == 11, 'The maximum subsequence sum of array [4, -3, 5, -2, -1, 2, 6, -2] is 11.');
+    ok(solution3([-2, 1, -3, 4, -1, 2, 1, -5, 4], 0, 8) == 6, 'The maximum subsequence sum of array [−2, 1, −3, 4, −1, 2, 1, −5, 4] is 6.');
+    ok(solution3([-2, -3, 4, -1, -2, 1, 5, -3], 0, 7) == 7, 'The maximum subsequence sum of array [-2, -3, 4, -1, -2, 1, 5, -3] is 7.');
+});
