b810b6b5dae17c3709b3b219af5ebb8f34fe849b
Frederick-S <mao_xiaodan@hotmail.com>
2015-07-24 16:15:22 +0800

    Update exercise

--- docs/mathjaxconfig.js
+++ docs/mathjaxconfig.js
@@ -1,3 +1,6 @@
 MathJax.Hub.Config({
-    tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}
+    tex2jax: {
+        inlineMath: [['$','$'], ['\\(','\\)']],
+        displayMath: [['$$','$$'], ["\\[","\\]"]]
+    }
 });
